ab7b97c0f1d0a86d83014db35a403dd6cbc2506e
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 10:15:02 +0300

    added user model

--- friendbook/models.py
+++ friendbook/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user_name = models.CharField(max_length =30)
+    profilephoto = models.ImageField(upload_to = 'images/')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        ordering = ['user_name']
+
+class Blog_Images(models.Model):
+    title = models.CharField(max_length =60)
+    i_images = models.ImageField(upload_to = 'images/')
+    caption = models.TextField()
+    locations = models.ForeignKey(Locations, on_delete=models.CASCADE,)

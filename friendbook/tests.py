ded10ee35e6e5d99d5723e2a2d8913df3d735619
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 10:48:44 +0300

    add save testing for friend_images

--- friendbook/tests.py
+++ friendbook/tests.py
@@ -45,8 +45,14 @@ class Friend_ImagesTestClass(TestCase):
             self.abuga= Profile(user_name = 'Abuga', profilephoto= 'testimage', bio='testbio of this user')
             self.abuga.save_profile()
 
-            self.abugaimage= Blog_Images(title= 'Cool pic',i_images='image',  caption='Lorem ipsum dolor sit amet.', profile= self.abuga, comments='Lorem ipsum dolor sit amet.', likes=0)
+            self.abugaimage= Friend_Images(title= 'Cool pic',i_images='image',  caption='Lorem ipsum dolor sit amet.', profile= self.abuga, comments='Lorem ipsum dolor sit amet.', likes=0)
 
         # Testing  instance
         def test_instance(self):
             self.assertTrue(isinstance(self.abugaimage,Friend_Images))
+
+        # Testing Save Method
+        def test_save_method(self):
+            self.abugaimage.save_image()
+            testsaving = Friend_Images.objects.all()
+            self.assertTrue(len(testsaving) > 0)            

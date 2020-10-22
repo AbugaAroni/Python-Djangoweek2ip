1fb756bffab1b2d6c824e8639b0215bbb1510474
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 10:40:38 +0300

    add first tests

--- friendbook/tests.py
+++ friendbook/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 from .models import Profile, Friend_Images
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.abuga= Profile(user_name = 'Abuga', profilephoto= 'testimage', bio='testbio of this user')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abuga,Profile))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.abuga.save_profile()
+        testsaved = Profile.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.abuga.save_profile()
+        testsaved = Profile.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+        self.abuga.delete_profile()
+        testdelete = Profile.objects.filter(user_name='Abuga')
+        self.assertEqual(len(cats), 0)

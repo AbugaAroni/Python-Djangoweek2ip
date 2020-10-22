c832f28040bc21af0dfee683a955153203dd9a98
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 10:25:02 +0300

    add images model

--- friendbook/models.py
+++ friendbook/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
+class Profile(models.Model):
     user_name = models.CharField(max_length =30)
     profilephoto = models.ImageField(upload_to = 'images/')
     bio = models.TextField()
@@ -13,8 +13,10 @@ class Users(models.Model):
     class Meta:
         ordering = ['user_name']
 
-class Blog_Images(models.Model):
+class Friend_Images(models.Model):
     title = models.CharField(max_length =60)
     i_images = models.ImageField(upload_to = 'images/')
     caption = models.TextField()
-    locations = models.ForeignKey(Locations, on_delete=models.CASCADE,)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    comments = models.TextField()
+    likes = models.IntegerField()

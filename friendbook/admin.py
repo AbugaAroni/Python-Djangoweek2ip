d8c6e38965d455008eeed2b85450a5bbb18edfee
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 11:26:45 +0300

    added working iapplication

--- friendbook/admin.py
+++ friendbook/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile, Friend_Images
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Friend_Images)
--- friendbook/views.py
+++ friendbook/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Profile, Friend_Images
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'homepage.html')
+    allimages = Friend_Images.objects.all()
+    allprofiles = Profile.objects.all()
+
+    return render(request, 'homepage.html', {"images":allimages})

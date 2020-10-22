6c8b91909a9c074f7467c136342e013016ca7454
AbugaAroni <abugaaroni@gmail.com>
2020-10-22 10:07:30 +0300

    create and add databse

--- ip2/settings.py
+++ ip2/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'ip2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'friendbook',
+        'USER': 'abuga',
+    'PASSWORD':'password',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
